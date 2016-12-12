bde5f9ada76b9ea50b7d6ace59b87a40f77b1629
Viktor Zhuromskyy <office@devdesco.com>
2016-12-12 15:57:23 -0600

    bring it up to date with PHP7 mysqli function
    mysql is obsolete in PHP7, so we use mysqli instead

--- change_mysql_definer_php7.php
+++ change_mysql_definer_php7.php
@@ -20,7 +20,6 @@ $new_definer_host = "bar";
 
 
 
-
 $old_definer_sql = "`".$old_definer_user."`@`".$old_definer_host."`";
 $old_definer=$old_definer_user."@".$old_definer_host;
 
@@ -35,39 +34,39 @@ function ddl($query)
     echo "Query: $query\n\n";
   }
   if ($for_real) {
-    mysql_query($query) or die(mysql_error());
+    mysqli_query($GLOBALS["___mysqli_ston"], $query) or die(((is_object($GLOBALS["___mysqli_ston"])) ? mysqli_error($GLOBALS["___mysqli_ston"]) : (($___mysqli_res = mysqli_connect_error()) ? $___mysqli_res : false)));
   }
 }
 
-mysql_connect($db_host, $db_user, $db_pass) or die("can't connect to mysql");
+($GLOBALS["___mysqli_ston"] = mysqli_connect($db_host,  $db_user,  $db_pass)) or die("can't connect to mysql");
 
 // modify views
 
 $get_views = "SELECT * FROM INFORMATION_SCHEMA.VIEWS WHERE DEFINER = '$old_definer'";
 
-$res = mysql_query($get_views) or die(mysql_error());
-while($row = mysql_fetch_assoc($res)) {
+$res = mysqli_query($GLOBALS["___mysqli_ston"], $get_views) or die(((is_object($GLOBALS["___mysqli_ston"])) ? mysqli_error($GLOBALS["___mysqli_ston"]) : (($___mysqli_res = mysqli_connect_error()) ? $___mysqli_res : false)));
+while($row = mysqli_fetch_assoc($res)) {
   echo "modifying view $row[TABLE_SCHEMA].$row[TABLE_NAME]\n";
 
-  mysql_select_db($row["TABLE_SCHEMA"]);
+  ((bool)mysqli_query($GLOBALS["___mysqli_ston"], "USE " . $row["TABLE_SCHEMA"]));
 
   $set = "SET SESSION character_set_client='$row[CHARACTER_SET_CLIENT]', collation_connection='$row[COLLATION_CONNECTION]'";
   $alter = "ALTER DEFINER=$new_definer_sql VIEW `$row[TABLE_NAME]` AS $row[VIEW_DEFINITION]";
-  
+
   ddl($set);
   ddl($alter);
 }
-mysql_free_result($res);
+((mysqli_free_result($res) || (is_object($res) && (get_class($res) == "mysqli_result"))) ? true : false);
 
 // modify triggers
 
 $get_triggers = "SELECT * FROM INFORMATION_SCHEMA.TRIGGERS WHERE DEFINER = '$old_definer'";
 
-$res = mysql_query($get_triggers) or die(mysql_error());
-while($row = mysql_fetch_assoc($res)) {
+$res = mysqli_query($GLOBALS["___mysqli_ston"], $get_triggers) or die(((is_object($GLOBALS["___mysqli_ston"])) ? mysqli_error($GLOBALS["___mysqli_ston"]) : (($___mysqli_res = mysqli_connect_error()) ? $___mysqli_res : false)));
+while($row = mysqli_fetch_assoc($res)) {
   echo "modifying trigger $row[TRIGGER_SCHEMA].$row[TRIGGER_NAME] on table $row[EVENT_OBJECT_SCHEMA].$row[EVENT_OBJECT_TABLE]\n";
 
-  mysql_select_db($row["TRIGGER_SCHEMA"]);
+  ((bool)mysqli_query($GLOBALS["___mysqli_ston"], "USE " . $row["TRIGGER_SCHEMA"]));
 
   $lock = "LOCK TABLES `$row[EVENT_OBJECT_TABLE]` WRITE";
   $unlock = "UNLOCK TABLES";
@@ -76,9 +75,9 @@ while($row = mysql_fetch_assoc($res)) {
 
   $show = "SHOW CREATE TRIGGER `$row[TRIGGER_NAME]`";
 
-  $res2 = mysql_query($show) or die($show.":".mysql_error());
-  $row2 = mysql_fetch_assoc($res2);
-  mysql_free_result($res2);
+  $res2 = mysqli_query($GLOBALS["___mysqli_ston"], $show) or die($show.":".((is_object($GLOBALS["___mysqli_ston"])) ? mysqli_error($GLOBALS["___mysqli_ston"]) : (($___mysqli_res = mysqli_connect_error()) ? $___mysqli_res : false)));
+  $row2 = mysqli_fetch_assoc($res2);
+  ((mysqli_free_result($res2) || (is_object($res2) && (get_class($res2) == "mysqli_result"))) ? true : false);
 
   $set = "SET SESSION character_set_client='$row[CHARACTER_SET_CLIENT]', collation_connection='$row[COLLATION_CONNECTION]'";
   $sql_mode = "SET SESSION SQL_MODE = '$row2[sql_mode]'";
@@ -91,14 +90,14 @@ while($row = mysql_fetch_assoc($res)) {
   ddl($drop);
   ddl($create);
   ddl($unlock);
-}  
+}
 
 // modify events
 
 $get_events = "SELECT * FROM INFORMATION_SCHEMA.EVENTS WHERE DEFINER = '$old_definer'";
 
-$res = mysql_query($get_events) or die(mysql_error());
-while($row = mysql_fetch_assoc($res)) {
+$res = mysqli_query($GLOBALS["___mysqli_ston"], $get_events) or die(((is_object($GLOBALS["___mysqli_ston"])) ? mysqli_error($GLOBALS["___mysqli_ston"]) : (($___mysqli_res = mysqli_connect_error()) ? $___mysqli_res : false)));
+while($row = mysqli_fetch_assoc($res)) {
   echo "modifying event $row[EVENT_SCHEMA].$row[EVENT_NAME]\n";
 
   $set = "SET SESSION character_set_client='$row[CHARACTER_SET_CLIENT]', collation_connection='$row[COLLATION_CONNECTION]'";
@@ -114,19 +113,19 @@ while($row = mysql_fetch_assoc($res)) {
 
 $get_procedures = "SELECT * FROM INFORMATION_SCHEMA.ROUTINES WHERE DEFINER = '$old_definer'";
 
-$res = mysql_query($get_procedures) or die(mysql_error());
-while($row = mysql_fetch_assoc($res)) {
+$res = mysqli_query($GLOBALS["___mysqli_ston"], $get_procedures) or die(((is_object($GLOBALS["___mysqli_ston"])) ? mysqli_error($GLOBALS["___mysqli_ston"]) : (($___mysqli_res = mysqli_connect_error()) ? $___mysqli_res : false)));
+while($row = mysqli_fetch_assoc($res)) {
   echo "modifying ".strtolower($row['ROUTINE_TYPE'])." $row[ROUTINE_SCHEMA].$row[ROUTINE_NAME]\n";
 
-  mysql_select_db($row["ROUTINE_SCHEMA"]);
+  ((bool)mysqli_query($GLOBALS["___mysqli_ston"], "USE " . $row["ROUTINE_SCHEMA"]));
 
   $drop = "DROP $row[ROUTINE_TYPE] `$row[ROUTINE_NAME]`";
 
   $show = "SHOW CREATE $row[ROUTINE_TYPE] `$row[ROUTINE_NAME]`";
 
-  $res2 = mysql_query($show) or die($show.":".mysql_error());
-  $row2 = mysql_fetch_assoc($res2);
-  mysql_free_result($res2);
+  $res2 = mysqli_query($GLOBALS["___mysqli_ston"], $show) or die($show.":".((is_object($GLOBALS["___mysqli_ston"])) ? mysqli_error($GLOBALS["___mysqli_ston"]) : (($___mysqli_res = mysqli_connect_error()) ? $___mysqli_res : false)));
+  $row2 = mysqli_fetch_assoc($res2);
+  ((mysqli_free_result($res2) || (is_object($res2) && (get_class($res2) == "mysqli_result"))) ? true : false);
 
   $set = "SET SESSION character_set_client='$row[CHARACTER_SET_CLIENT]', collation_connection='$row[COLLATION_CONNECTION]'";
 
